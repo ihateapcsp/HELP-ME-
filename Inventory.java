c6ef53252526351052c5f71ed56d7bda9d7b1456
GOKUSOLOS900 <ChangGregory6485@students.esuhsd.org>
2025-01-24 09:50:20 -0800

    Update Inventory.java

--- Inventory.java
+++ Inventory.java
@@ -1,1 +1,10 @@
+import java.util.ArrayList;
+public class Inventory{
+  private ArrayList<Item> items;
 
+  public void addItem(Item item){
+    items.add(item);
+  }
+  public Item findItem(String itemID){
+  }
+}
