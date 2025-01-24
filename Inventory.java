ff18708ffa1a790a234652758a35c7364c9e744c
ihateapcsp <nguyenchristophe6531@students.esuhsd.org>
2025-01-24 10:10:34 -0800

    Update Inventory.java

--- Inventory.java
+++ Inventory.java
@@ -1,18 +1,22 @@
+
 import java.util.ArrayList;
 public class Inventory{
   private ArrayList<Item> items;
 
+  public Inventory(){
+      this.items = new ArrayList<>();
+  }
   public void addItem(Item item){
     items.add(item);
   }
-  public Item findItem(String findItemID){
-    for (int i=0;i<items.size();i++){
-      Item itemstack=items.get(i);
-      if (itemstack.itemID.equals(findItemID)){
-        return itemstack;
+  public Item findItem(String findItemID) {
+      for (int i = 0; i < items.size(); i++) {
+          Item itemstack = items.get(i);
+          if (itemstack.itemID.equals(findItemID)) {
+              return itemstack;
+          }
       }
-    }
-    return null;
+      return null; 
   }
   public void displayAllItems(){
     for (int i=0;i<items.size();i++){
